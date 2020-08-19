81898ffbc11f289aacc8faa75ef441bdf53cd55b
Jason Nyland <jason@jasonnyland.com>
2020-08-18 19:48:21 -0700

    refactored to let/const and arrow functions, remove unused variables

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 require('./models.js');
-var bcrypt = require('bcrypt');
-var expressSession = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
+const expressSession = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const dotenv = require('dotenv');
 dotenv.config();
-var AWS = require('aws-sdk');
+//const AWS = require('aws-sdk');
 //AWS.config.update({region: 'REGION'}); // Load credentials and set region from JSON file
-var ec2 = require('./ec2.js');
-var User = mongoose.model('User');
-var namecheap = require('./namecheap.js');
-var sshscript = require('./sshscript.js');
-
+const ec2 = require('./ec2.js');
+const User = mongoose.model('User');
+const namecheap = require('./namecheap.js');
+const sshscript = require('./sshscript.js');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-mongoose.connect('mongodb://'+process.env.MONGO_USERNAME+":"+process.env.MONGO_PASSWORD+"@"+process.env.MONGO_LOCATION,{useNewUrlParser: true, useUnifiedTopology: true})
-//mongoose.connect(process.env.MONGO_LOCATION, {useNewUrlParser: true, useUnifiedTopology: true});
 
-var app = express();
+
+//mongoose.connect('mongodb://'+process.env.MONGO_USERNAME+":"+process.env.MONGO_PASSWORD+"@"+process.env.MONGO_LOCATION,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_LOCATION, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,15 +34,13 @@ app.set('view engine', 'ejs');
 const bodyParser = require('body-parser');
 
 // Match the raw body to content type application/json
-app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
-    const sig = request.headers['stripe-signature'];
-
+app.post('/webhook', bodyParser.raw({type: 'application/json'}), (req, res) => {
+    const sig = req.headers['stripe-signature'];
     let event;
-
     try {
-        event = stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_ENDPOINT_SECRET);
+        event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_ENDPOINT_SECRET);
     } catch (err) {
-        return response.status(400).send(`Webhook Error: ${err.message}`);
+        return res.status(400).send(`Webhook Error: ${err.message}`);
     }
 
     // Handle the checkout.session.completed event
@@ -52,13 +51,13 @@ app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, respo
         console.log("[Checkout Completed]");
         User.findOne({
             email: session.customer_email
-        }, function (err, user) {
+        }, (err, user) => {
             if (user) {
                 user.subscriptionActive = true;
                 user.subscriptionId = session.subscription;
                 user.customerId = session.customer;
                 user.save();
-                ec2.newEC2(function (err, data) {
+                ec2.newEC2((err, data) => {
                     console.log("[AWS API] New instance created:", data);
                     user.instance = data;
                     user.save();
@@ -69,7 +68,7 @@ app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, respo
     }
 
     // Return a response to acknowledge receipt of the event
-    response.json({received: true});
+    res.json({received: true});
 });
 // ----------------- End Stripe Sample Code ------------------
 
@@ -89,54 +88,53 @@ app.use(passport.session());
 passport.use(new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
-}, function (email, password, next) {
+}, (email, password, next) => {
     User.findOne({
         email: email
-    }, function (err, user) {
+    }, (err, user) => {
         if (err) return next(err);
         if (!user || !bcrypt.compareSync(password, user.passwordHash)) {
             return next({message: 'Email or password incorrect'});
         }
-        next(null, user);
+        return next(null, user);
     })
 }));
 
 passport.use('signup-local', new LocalStrategy({
     usernameField: 'email',
-    passwordField: 'password',
-    passwordField: 'password',
-}, function (email, password, next) {
+    passwordField: 'password'
+}, (email, password, next) => {
     User.findOne({
         email: email
-    }, function (err, user) {
+    }, (err, user) => {
         if (err) return next(err);
         if (user) return next({message: "User already exists"});
         let newUser = new User({
             email: email,
             passwordHash: bcrypt.hashSync(password, 10)
         });
-        newUser.save(function (err) {
-            next(err, newUser);
+        newUser.save((err) => {
+            return next(err, newUser);
         });
 
     });
 }));
 
-passport.serializeUser(function (user, next) {
-    next(null, user._id);
+passport.serializeUser((user, next) => {
+    return next(null, user._id);
 });
 
-passport.deserializeUser(function (id, next) {
-    User.findById(id, function (err, user) {
-        next(err, user);
+passport.deserializeUser((id, next) => {
+    User.findById(id, (err, user) => {
+        return next(err, user);
     })
 });
 
-app.get('/', function (req, res, next) {
+app.get('/', (req, res) => {
     res.render('index', {title: "RobotKeys"});
 });
 
-app.get('/billing', function (req, res, next) {
+app.get('/billing', (req, res, next) => {
     if (req.user.subscriptionActive) {
         res.redirect('/main');
     } else {
@@ -150,7 +148,7 @@ app.get('/billing', function (req, res, next) {
             },
             success_url: process.env.STRIPE_WEBHOOK_SUCCESS_URL,
             cancel_url: process.env.STRIPE_WEBHOOK_CANCEL_URL,
-        }, function (err, session) {
+        }, (err, session) => {
             if (err) return next(err);
             res.render('billing', {
                 STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
@@ -162,12 +160,12 @@ app.get('/billing', function (req, res, next) {
     }
 });
 
-app.get('/logout', function (req, res, next) {
+app.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
 });
 
-app.get('/main', function (req, res, next) {
+app.get('/main', (req, res) => {
     if (!req.user.subscriptionActive) {
         res.redirect('/billing');
     } else {
@@ -181,38 +179,38 @@ app.get('/main', function (req, res, next) {
 
 });
 
-app.post('/main', function (req, res, next) {
+app.post('/main', (req, res, next) => {
     // if user has an instance, get the IP, update nameservers, and launch scripts when nameservers update
     if (req.user.instance) {
-        User.findOne({subdomain: req.body.subdomain}, function (err, user) {
+        User.findOne({subdomain: req.body.subdomain}, (err, user) => {
             if (err) return next(err);
             if (user) return next({message: 'Subdomain is taken.'});
             if (!user) {
-                User.findOne({_id: req.user._id}, function (err, user) {
+                User.findOne({_id: req.user._id}, (err, user) => {
                     if (err) return next(err);
                     if (user) {
                         user.subdomain = req.body.subdomain;
                         user.save();
                         /////////////////////////////////////////////
-                        ec2.getIP(req.user.instance, function (err, data) {
+                        ec2.getIP(req.user.instance, (err, data) => {
                             user.ip = data;
                             user.save();
-                            namecheap.addHost(user.subdomain, user.ip, function (err, data) {
-                                var entry = {
+                            namecheap.addHost(user.subdomain, user.ip, (err, data) => {
+                                let entry = {
                                     domain: [user.subdomain, process.env.APP_URL_SLD, process.env.APP_URL_TLD].join('.'),
                                     dav_user: req.body.name,
                                     dav_pass: req.body.pw
                                 }
-                                sshscript.dnsWatchdog(entry.domain, 0, function () {
+                                sshscript.dnsWatchdog(entry.domain, 0, () => {
                                     console.log("Watchdog ended, triggering sshPayload")
-                                    sshscript.sshPayload(entry, function () {
+                                    sshscript.sshPayload(entry, () => {
                                         console.log("sshPayload has ended and triggered its callback")
                                     });
                                 });
                             });
                         });
                         /////////////////////////////////////////////
-                        ec2.tagInstance(user.instance,'Name',user.subdomain, function () {
+                        ec2.tagInstance(user.instance,'Name',user.subdomain, () => {
                             //console.log("Instance Tagged")
                         })
                         res.redirect('/main');
@@ -224,42 +222,33 @@ app.post('/main', function (req, res, next) {
 
 });
 
-app.get('/login', function (req, res, next) {
+app.get('/login', (req, res) => {
     res.render('login');
 });
 
 app.post('/login',
     passport.authenticate('local', {failureRedirect: '/login'}),
-    function (req, res) {
+    (req, res) => {
         res.redirect('/main');
     });
 
-app.get('/guide', function (req, res, next) {
+app.get('/guide', (req, res) => {
     res.render('guide')
 });
 
-// app.get('/guide', function (req,res,next) {
-//   req.session.sawWalkthrough = true;
-//   res.end();
-// });
-//
-// app.get('/complicated', function (req,res,next) {
-//   console.log(req.session.sawWalkthrough);
-// });
-
 app.post('/signup',
     passport.authenticate('signup-local', {failureRedirect: '/'}),
-    function (req, res) {
+    (req, res) => {
         res.redirect('/billing');
     });
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
